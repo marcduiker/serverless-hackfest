231769ff5adbc3ba4dd1f2019a4df58fb67db101
Marc Duiker <marcduiker@gmail.com>
2018-02-06 15:16:51 +0100

    Added CosmosDB binding

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Serverless Hackfest
+
+## Goal
+
+Make end to end serverless application
+
+## Ingredients
+
+- Web app (for user input + image upload)
+- Storage Queue
+- Azure Functions
+- EventGrid
+- Logic Apps
+- Cognitive Services
+- CosmosDB
+
+use case: web app met user input en image
--- src/HackfestApp/HackfestApp/Start.cs
+++ src/HackfestApp/HackfestApp/Start.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,26 +12,22 @@ namespace HackfestApp
     public static class Start
     {
         [FunctionName("Start")]
-        public static async Task<HttpResponseMessage> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, 
+        public static HttpResponseMessage Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req,
+            [DocumentDB("Data", "xpiritserverlessdata", ConnectionStringSetting = "CosmosDBConnection")] out dynamic document,
             TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            // parse query parameter
-            string name = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
-                .Value;
+           // Get request body
+            dynamic data = req.Content.ReadAsAsync<object>();
+            var name = data?.name;
 
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
-
-            // Set name to query string or body data
-            name = name ?? data?.name;
+            document = new { Name = name, id = Guid.NewGuid() };
 
             return name == null
                 ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+                : req.CreateResponse(HttpStatusCode.OK);
         }
     }
 }
